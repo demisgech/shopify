28672c36f64afd66494d0f1a2761b8f232eb2d36
Demis Getachew <demisgech@gmail.com>
2025-03-10 11:43:29 +0300

    Buld fully responsive mobile first navbar

--- src/Pages/routes.tsx
+++ src/Pages/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import ProductPage from "./ProductPage";
 import Layout from "./Layout";
+import CartPage from "./CartPage";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,11 @@ const router = createBrowserRouter([
         path: "/products",
 
         element: <ProductPage />,
+      },
+      {
+        path: "/carts",
+
+        element: <CartPage />,
       },
     ],
   },
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,88 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import cartLogo from "../assets/cartlogo.png";
+
 const Navbar = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <nav className="navbar bg-purple-400 col-span-full text-primary-content justify-between">
-      <div className="flex-1 px-2">
-        <span className="text-lf font-bold">Navigation</span>
+    <nav className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1 gap-2">
+        <Link to="/" className="btn btn-ghost text-xl">
+          Shopify
+        </Link>
       </div>
+
+      <div className="hidden lg:flex space-x-6">
+        <Link to="/">Home</Link>
+        <Link to="/carts">Cart</Link>
+        <Link to="/products">Products</Link>
+        <div className="bordered">
+          <input
+            type="search"
+            className="input outline-0"
+            placeholder="Search ..."
+          />
+        </div>
+      </div>
+
+      <Link to="/carts" className="border-1 border-cyan-200 rounded-sm mx-4">
+        <div className="indicator">
+          <img
+            src={cartLogo}
+            alt="cart logo"
+            className="w-[30px] h-[30px] object-cover"
+          />
+          <span className="badge badge-error badge-sm text-white rounded-full indicator-item">
+            8
+          </span>
+        </div>
+      </Link>
       <div className="flex-none lg:hidden">
-        <button className="btn btn-primary btn-sm">☰ toggle</button>
+        <button
+          className="btn btn-primary btn-sm"
+          onClick={() => setOpen(!isOpen)}
+        >
+          ☰
+        </button>
       </div>
+      {isOpen && (
+        <ul className="menu menu-sm absolute top-16 right-1 shadow-md p-4 w-50 bg-white rounded-lg z-10 lg:hidden">
+          <li>
+            <Link to="/" className="block py-2" onClick={() => setOpen(false)}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/carts"
+              className="block py-2"
+              onClick={() => setOpen(false)}
+            >
+              Cart
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/products"
+              className="block py-2"
+              onClick={() => setOpen(false)}
+            >
+              Products
+            </Link>
+          </li>
+          <li>
+            <div className="bordered">
+              <input
+                type="search"
+                className="input outline-0"
+                placeholder="Search ..."
+              />
+            </div>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
