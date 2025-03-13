677823d38248b2d1bd02f5682327762f891ff5ae
Demis Getachew <demisgech@gmail.com>
2025-03-13 14:54:10 +0300

    Build home page

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,3 +1,31 @@
+import banner from "../assets/wallpaper-ecommerce-web-portal.webp";
+import Footer from "../components/Footer";
+
 export default function HomePage() {
-  return <h1 className="text-6xl text-sky-300">HomePage</h1>;
+  return (
+    <div>
+      <section className="m-auto bg-base-200 shadow-2xl rounded-2xl p-2 lg:m-10">
+        <div className="flex justify-center items-center bg-base-200 min-h-screen lg:w-[90vw]">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+            <img src={banner} className="rounded-lg max-w-[100%]" />
+            <div>
+              <h1 className="text-6xl bg-linear-to-bl from-violet-800 to-green-300 text-blue-650 bg-clip-text text-transparent sm:mb-5">
+                Shop Smarter, Live Better – Discover the Best Deals Today!
+              </h1>
+              <p className="py-6">
+                Find the best deals on top-quality products at unbeatable
+                prices. Shop smarter with curated collections, exclusive
+                discounts, and fast shipping—because you deserve the best
+                without overspending!
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <Footer />
+      </section>
+    </div>
+  );
 }
